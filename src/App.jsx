f56e80f02fa2fa8c3c0f7f6987d96cdf43271c61
pifragile <pigu@bluewin.ch>
2023-10-17 19:04:05 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from "./assets/logo.png";
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <img src={logo} className="logo" alt="logo" />
+        </>
+    );
+}
+
+export default App;
